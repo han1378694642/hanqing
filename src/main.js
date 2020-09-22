54c06cc015474c214203e1828ee32faad2c8e83b
han1378694642 <1378694642@qq.com>
2020-09-22 19:47:39 +0800

    最后一次提交

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import './assets/fonts/iconfont.css'
 import axios from 'axios'
 // 配置请求的根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
-Vue.prototype.$http=axios
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
